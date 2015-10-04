79d314b1962de16d7b8bc81f4d5f6af387a73738
David Overeem <dovereem@cox.net>
2015-10-04 01:26:35 -0700

    added licensing, touches almost every file.  also put the license template in the netbeans properties

--- test/org/omg/space/xtce/tests/ContainerDecodingTest.java
+++ test/org/omg/space/xtce/tests/ContainerDecodingTest.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Copyright 2015 David Overeem (dovereem@cox.net)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  */
 
 package org.omg.space.xtce.tests;
@@ -12,7 +23,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 import junit.framework.Assert;
@@ -24,11 +34,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.omg.space.xtce.toolkit.XTCEContainerContentEntry;
 import org.omg.space.xtce.toolkit.XTCEContainerContentModel;
-import org.omg.space.xtce.toolkit.XTCEContainerEntryValue;
 import org.omg.space.xtce.toolkit.XTCEDatabase;
 import org.omg.space.xtce.toolkit.XTCEDatabaseException;
 import org.omg.space.xtce.toolkit.XTCEFunctions;
-import org.omg.space.xtce.toolkit.XTCEParameter;
 import org.omg.space.xtce.toolkit.XTCETMContainer;
 
 /**
